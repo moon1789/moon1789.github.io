513399476cddd715f84159f22c847314361c9ebb
moon1789 <72432771+moon1789@users.noreply.github.com>
2021-09-25 09:13:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function down() {
     document.getElementById("menu").style.bottom = "-20vh"
     document.getElementById("ha").style.bottom = "0"
 }
+function menu() {
+    document.getElementById("menu").style.bottom = "0";
+    document.getElementById("header").style.top = "0";
+}
+function port() {
+    document.getElementById("menu").style.bottom = "0";
+    document.getElementById("header").style.top = "0";
+}
+
 
 const text = document.getElementById("text")
 const button = document.getElementById("butoun")
@@ -19,6 +28,40 @@ function randomQuote() {
 }
 
 randomQuote();
-button.addEventListener('click', () => {
-    randomQuote();
-});
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.right = "20px";
+    } else {
+        mybutton.style.right = "-40vh";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+var textArray = [
+    'Marhaba!',
+    'Namaskar!',
+    'Hola!',
+    'Bonjour!',
+    'Namaste!',
+    'Olá',
+    'Hola',
+    'Salam!',
+    'Hello!',
+
+
+];
+console.log("dont dare to look at the source code , its a mess, ty.")
+var randomNumber = Math.floor(Math.random() * textArray.length);
+
+document.getElementById("hello").innerHTML = ('src', textArray[randomNumber]);
